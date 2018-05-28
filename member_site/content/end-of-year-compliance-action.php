7e25844ed7943e3410502c799619a2dddbaaa914
Lawton Mizell <alcamech@gmail.com>
2018-05-28 14:40:11 -0400

    submitting values to db

--- member_site/content/end-of-year-compliance-action.php
+++ member_site/content/end-of-year-compliance-action.php
@@ -1,38 +1,53 @@
-<html>
-<body>
-  Name: <?php echo $_POST["name"]; ?><br>
-  Your email address is: <?php echo $_POST["email"]; ?><br>
-  Your phone number is: <?php echo $_POST["phone"]; ?><br>
-  You have selected :
-  <?php
-  if (isset($_POST['submit'])) {
-    if(isset($_POST['gridRadios']))
-    {
-      echo $_POST['gridRadios'];  //  Displaying Selected Value
-    }
+<?php
+//personal information
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$name = $_POST['name'];
+
+//initials
+$initial_1 = $_POST['initial1'];
+$initial_2 = $_POST['initial2'];
+$initial_3 = $_POST['initial3'];
+$initial_4 = $_POST['initial4'];
+$initial_5 = $_POST['initial5'];
+$initial_6 = $_POST['initial6'];
+$initial_7 = $_POST['initial7'];
+
+//submission
+if (isset($_POST['submit'])) {
+  if(isset($_POST['submission']))
+  {
+    $submitted_worksheet = $_POST['submission'];  //  Displaying Selected Value
   }
-  ?><br>
-  You have selected :
-  <?php
-  if (isset($_POST['submit'])) {
-    if(isset($_POST['options']))
-    {
-      echo $_POST['options'];  //  Displaying Selected Value
-    }
+}
+
+//dual enrollment
+if (isset($_POST['submit'])) {
+  if(isset($_POST['dual-enrollment']))
+  {
+    $dual_enrollment = $_POST['dual-enrollment'];  //  Displaying Selected Value
   }
-  ?><br>
-  You have selected :
-  <?php
-  if (isset($_POST['submit'])) {
-    if(isset($_POST['optionsRadios']))
-    {
-      if($_POST['optionsRadios'] == "school name") {
-        echo $_POST['optionsRadios'] . " : " . $_POST["school"];
-      } else {
-        echo $_POST['optionsRadios'];  //  Displaying Selected Value
-      }
-    }
-  }
-  ?>
-</body>
-</html>
+}
+
+// Establish MySQL connection
+$con=mysqli_connect("127.0.0.1","root","newpassword","FaithfulScholars");
+if(mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+};
+
+// Compliance Information
+// SQL Queries //TODO: Replace with prepared statements after login is merged in
+$family_id = 1;
+$sql = "INSERT INTO eoy (family_id, initial_1, initial_2, initial_3, initial_4, initial_5,
+                         initial_6, initial_7, submitted_worksheet, dual_enrollment)
+                    VALUES('$family_id','$initial_1','$initial_2','$initial_3','$initial_4',
+                           '$initial_5','$initial_6','$initial_7','$submitted_worksheet',
+                            '$dual_enrollment')";
+if ($con->query($sql) === TRUE) {
+  echo "New record created<br>";
+  $insert_id = mysqli_insert_id($con);
+  echo $insert_id . "<br>";
+} else {
+  echo "Error: " . $sql . "<br>" . $con->error . "<br>";
+}
+?>
