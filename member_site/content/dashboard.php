ce111e7e649b4886311884d11ca97323b275e0bc
Tyler Moon <moon.tyler@gmail.com>
2018-06-05 19:58:24 -0400

    Fixed styling issues on the dashboard and made site more mobile responsive

--- member_site/content/dashboard.php
+++ member_site/content/dashboard.php
@@ -1,88 +1,102 @@
 <div class="container">
   <div class="row">
     <div class="col-md-12 documents btn-group">
-      <div class="row">
-        <div class="col-md-12">
-          <h3>Documents</h3>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h3>Documents</h3>
+          </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="col-md-4">
-          <a class="undecorated" href="/content/membership-letter.php">
-            <div class="circle center">
-              Membership Letter
-            </div>
-          </a>
-        </div>
-        <div class="col-md-4">
-          <a class="undecorated" href="/content/dmv-letter.php">
-            <div class="circle center">
-              DMV Letter
-            </div>
-          </a>
-        </div>
-        <div class="col-md-4">
-          <a class="undecorated" href="/?page=membership-letter">
-            <div class="circle center">
-              Secondary School Form
-            </div>
-          </a>
+        <div class="row">
+          <div class="col-md-4">
+            <a class="undecorated" href="/content/membership-letter.php">
+              <div class="circle center">
+                Membership Letter
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4">
+            <a class="undecorated" href="/content/dmv-letter.php">
+              <div class="circle center">
+                DMV Letter
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4">
+            <a class="undecorated" href="/?page=membership-letter">
+              <div class="circle center">
+                Secondary School Form
+              </div>
+            </a>
+          </div>
         </div>
       </div>
     </div>
   </div>
   <div class="row">
     <div class="col-md-12 endofyear btn-group">
-      <h3>End of Year Forms</h3>
-      <div class="row">
-        <div class="col-md-4">
-          <a class="undecorated" href="/?page=renewal">
-            <div class="circle center">
-              Renewal
-            </div>
-          </a>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h3>End of Year Forms</h3>
+          </div>
         </div>
-        <div class="col-md-4">
-          <a class="undecorated" href="/?page=membership-letter">
-            <div class="circle center">
-              Mid Year Compliance
-            </div>
-          </a>
-        </div>
-        <div class="col-md-4">
-          <a class="undecorated" href="/?page=end-of-year-compliance">
-            <div class="circle center">
-              End of Year Compliance
-            </div>
-          </a>
+        <div class="row">
+          <div class="col-md-4">
+            <a class="undecorated" href="/?page=renewal">
+              <div class="circle center">
+                Renewal
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4">
+            <a class="undecorated" href="/?page=membership-letter">
+              <div class="circle center">
+                Mid Year Compliance
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4">
+            <a class="undecorated" href="/?page=end-of-year-compliance">
+              <div class="circle center">
+                End of Year Compliance
+              </div>
+            </a>
+          </div>
         </div>
       </div>
     </div>
   </div>
   <div class="row">
     <div class="col-md-12 transcripts btn-group">
-      <h3>Transcripts</h3>
-      <div class="row">
-        <div class="col-md-4">
-          <a class="undecorated" href="http://www.faithfulscholars.com/middle-school-transcript/">
-            <div class="circle center">
-              Middle School Transcript Input
-            </div>
-          </a>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h3>Transcripts</h3>
+          </div>
         </div>
-        <div class="col-md-4">
-          <a class="undecorated" href="http://www.faithfulscholars.com/high-school-transcript/">
-            <div class="circle center">
-              High School Transcript Input
-            </div>
-          </a>
-        </div>
-        <div class="col-md-4">
-          <a class="undecorated" href="http://www.faithfulscholars.com/request-a-transcript/">
-            <div class="circle center">
-              Request Mailed Transcript
-            </div>
-          </a>
+        <div class="row">
+          <div class="col-md-4">
+            <a class="undecorated" href="http://www.faithfulscholars.com/middle-school-transcript/">
+              <div class="circle center">
+                Middle School Transcript Input
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4">
+            <a class="undecorated" href="http://www.faithfulscholars.com/high-school-transcript/">
+              <div class="circle center">
+                High School Transcript Input
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4">
+            <a class="undecorated" href="http://www.faithfulscholars.com/request-a-transcript/">
+              <div class="circle center">
+                Request Mailed Transcript
+              </div>
+            </a>
+          </div>
         </div>
       </div>
     </div>
