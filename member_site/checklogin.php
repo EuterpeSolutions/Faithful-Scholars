74b6c18daa459bc63533778a713dd47c43e99ca1
Tyler Moon <moon.tyler@gmail.com>
2018-06-19 18:45:07 -0400

    Fixed errors in join form

--- member_site/checklogin.php
+++ member_site/checklogin.php
@@ -11,8 +11,8 @@ ob_start();
 // Connect to server and select databse.
 $con = db_connect();
 // Define $myusername and $mypassword
-$myusername=$_POST['myusername'];
-$mypassword=$_POST['mypassword'];
+$myusername=$_POST['username'];
+$mypassword=$_POST['password'];
 
 // To protect MySQL injection
 $myusername = stripslashes($myusername);
--- member_site/dbconfig.php
+++ member_site/dbconfig.php
@@ -1,8 +1,8 @@
 <?php
-$host="localhost"; // Host name
+$host="127.0.0.1"; // Host name
 $username="root"; // Mysql username
-$password=""; // Mysql password
-$db_name="mysql"; // Database name
+$password="newpassword"; // Mysql password
+$db_name="FaithfulScholars"; // Database name
 $tbl_name="members"; // Table name
 
 function db_connect(){
