0e2b16c8053655e6185b39e0f6161f9d7afd03f2
Tyler Moon <moon.tyler@gmail.com>
2018-06-23 14:43:27 -0400

    Added route to admin users

--- member_site/config.php
+++ member_site/config.php
@@ -1,4 +1,5 @@
 <?php
+require_once('dbconfig.php');
   // Used to store website configuration information
   function config($key = '')
   {
@@ -14,7 +15,21 @@
       'pretty_uri' => false,
       'stylesheet_path' => 'stylesheet',
     ];
-
+    if(isAdmin($_SESSION['uname']) == 1){
+      $config = [
+        'name' => 'Faithful Scholars',
+        'nav_menu' => [
+          '' => 'Dashboard',
+          'update-family-information' => 'Update Family Information',
+          'admin' => 'Admin Panel',
+          'logout' => 'Logout'
+        ],
+        'template_path' => 'template',
+        'content_path' => 'content',
+        'pretty_uri' => false,
+        'stylesheet_path' => 'stylesheet',
+      ];
+    }
     return isset($config[$key]) ? $config[$key] : null;
   }
 
