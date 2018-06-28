53496ece72fd29bda79908a060a12720fbece6a1
Tyler Moon <moon.tyler@gmail.com>
2018-06-27 21:14:55 -0400

    Changed background

--- member_site/content/dashboard.php
+++ member_site/content/dashboard.php
@@ -1,10 +1,44 @@
 <div class="container">
   <div class="row">
+    <div class="col-md-12 endofyear btn-group">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <h3>Forms (Everyone)</h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4">
+            <a class="undecorated" href="/member_site/?page=renewal">
+              <div class="circle center">
+                Renewal
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4">
+            <a class="undecorated" href="http://www.faithfulscholars.com/apply-online/">
+              <div class="circle center">
+                Join
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4">
+            <a class="undecorated" href="/member_site/?page=end-of-year-compliance">
+              <div class="circle center">
+                End of Year Compliance
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
     <div class="col-md-12 documents btn-group">
       <div class="container">
         <div class="row">
           <div class="col-md-12">
-            <h3>Documents</h3>
+            <h3>Documents (High Schoolers Only)</h3>
           </div>
         </div>
         <div class="row">
@@ -28,40 +62,6 @@
                 <div style="padding-top: 70px; line-height: 30px;">
                   Secondary School Form (College / Dual Credit)
                 </div>
-              </div>
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-md-12 endofyear btn-group">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-4">
-            <h3>End of Year Forms</h3>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-4">
-            <a class="undecorated" href="/member_site/?page=renewal">
-              <div class="circle center">
-                Renewal
-              </div>
-            </a>
-          </div>
-          <div class="col-md-4">
-            <a class="undecorated" href="/member_site/?page=end-of-year-compliance">
-              <div class="circle center">
-                Mid Year Compliance
-              </div>
-            </a>
-          </div>
-          <div class="col-md-4">
-            <a class="undecorated" href="/member_site/?page=end-of-year-compliance">
-              <div class="circle center">
-                End of Year Compliance
               </div>
             </a>
           </div>
